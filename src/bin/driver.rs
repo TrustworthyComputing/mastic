11e6f2cfbae3671a8a3253e814bf392a9a61aeef
Dimitris Mouris <jimouris@udel.edu>
2024-01-30 17:19:51 -0500

    Print FLP, JR, and nonce sizes

--- src/bin/driver.rs
+++ src/bin/driver.rs
@@ -68,7 +68,8 @@ fn generate_keys(
 fn generate_randomness(
     keys: (&Vec<VidpfKey>, &Vec<VidpfKey>),
 ) -> (Vec<[u8; 16]>, Vec<[[u8; 16]; 2]>) {
-    keys.0
+    let (nonces, jr_parts): (Vec<[u8; 16]>, Vec<[[u8; 16]; 2]>) = keys
+        .0
         .par_iter()
         .zip(keys.1.par_iter())
         .map(|(key_0, key_1)| {
@@ -92,7 +93,14 @@ fn generate_randomness(
 
             (nonce, jr_parts)
         })
-        .unzip()
+        .unzip();
+
+    let encoded: Vec<u8> = bincode::serialize(&nonces[0]).unwrap();
+    println!("Nonce size: {:?} bytes", encoded.len());
+    let encoded: Vec<u8> = bincode::serialize(&jr_parts[0]).unwrap();
+    println!("JR size: {:?} bytes", encoded.len());
+
+    (nonces, jr_parts)
 }
 
 fn generate_proofs(
@@ -100,7 +108,7 @@ fn generate_proofs(
     beta_values: &Vec<Vec<Field64>>,
     all_jr_parts: &Vec<[[u8; 16]; 2]>,
 ) -> (Vec<Vec<Field64>>, Vec<Vec<Field64>>) {
-    all_jr_parts
+    let (proofs_0, proofs_1): (Vec<Vec<Field64>>, Vec<Vec<Field64>>) = all_jr_parts
         .par_iter()
         .zip_eq(beta_values.par_iter())
         .map(|(jr_parts, input_beta)| {
@@ -124,7 +132,12 @@ fn generate_proofs(
 
             (proof_0, proof_1)
         })
-        .unzip()
+        .unzip();
+
+    let encoded: Vec<u8> = bincode::serialize(&proofs_0[0]).unwrap();
+    println!("FLP proof size: {:?} bytes", encoded.len());
+
+    (proofs_0, proofs_1)
 }
 
 async fn reset_servers(
